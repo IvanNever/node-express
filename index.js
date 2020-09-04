dc1a779c3efda33af3609491d4885579b53595cb
Ivan Neverovsky <ivan@neverovsky.com.ua>
2020-09-04 11:51:48 +0300

    form submit

--- index.js
+++ index.js
@@ -15,7 +15,10 @@ const hbs = exphbs.create({
 app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
 app.set('views', 'views')
+
 app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
+
 app.use('/', homeRoutes)
 app.use('/add', addRoutes)
 app.use('/services', servicesRoutes)
--- routes/add.js
+++ routes/add.js
@@ -8,4 +8,10 @@ router.get('/', (req, res) => {
     })
 })
 
+router.post('/', (req, res) => {
+    console.log(req.body)
+
+    res.redirect('/services')
+})
+
 module.exports = router
