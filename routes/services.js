07316935d64265f03f5457f7015836d6f53f8bca
Ivan Neverovsky <ivan@neverovsky.com.ua>
2020-09-04 13:07:18 +0300

    Services list output

--- routes/services.js
+++ routes/services.js
@@ -1,10 +1,14 @@
 const {Router} = require('express')
+const Service = require('../models/service')
 const router = Router()
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+    const services = await Service.getAll()
+
     res.render('services', {
         title: 'Services',
-        isServices: true
+        isServices: true,
+        services
     })
 })
 
