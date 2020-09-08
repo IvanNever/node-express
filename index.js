9e460ba5d3ddcc89b518e7b54d53be981a381ec8
Ivan Neverovsky <ivan@neverovsky.com.ua>
2020-09-08 17:25:44 +0300

    mail sender setup

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ const ordersRoutes = require('./routes/orders')
 const authRoutes = require('./routes/auth')
 const varMiddleware = require('./middleware/variables')
 const userMiddleware = require('./middleware/user')
+const keys = require('./keys')
 
-const MONGODB_URI = 'mongodb+srv://Ivan:OOtaqlg8WpHhSMzY@cluster0.4b0vv.mongodb.net/shop'
 const app = express()
 
 const hbs = exphbs.create({
@@ -28,7 +28,7 @@ const hbs = exphbs.create({
 
 const store = new MongoStore({
     collection: 'sessions',
-    uri: MONGODB_URI
+    uri: keys.MONGODB_URI
 })
 
 app.engine('hbs', hbs.engine)
@@ -38,7 +38,7 @@ app.set('views', 'views')
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({extended: true}))
 app.use(session({
-    secret: 'some secret value',
+    secret: keys.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
     store
@@ -60,7 +60,7 @@ const PORT = process.env.PORT || 3000
 
 async function start() {
     try {
-        await mongoose.connect(MONGODB_URI, {
+        await mongoose.connect(keys.MONGODB_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useFindAndModify: false
