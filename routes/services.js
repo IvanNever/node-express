35fbff00238fec348c54195857b8be7a4bc39c30
Ivan Neverovsky <ivan@neverovsky.com.ua>
2020-09-09 18:19:34 +0300

    access rights

--- routes/services.js
+++ routes/services.js
@@ -3,16 +3,25 @@ const Service = require('../models/service')
 const auth = require('../middleware/auth')
 const router = Router()
 
-router.get('/', async (req, res) => {
-    const services = await Service.find()
-        .populate('userId', 'email name')
-        .select('price title img')
+function isOwner(service, req) {
+    return service.userId.toString() === req.user._id.toString()
+}
 
-    res.render('services', {
-        title: 'Services',
-        isServices: true,
-        services
-    })
+router.get('/', async (req, res) => {
+    try {
+        const services = await Service.find()
+            .populate('userId', 'email name')
+            .select('price title img')
+
+        res.render('services', {
+            title: 'Services',
+            isServices: true,
+            userId: req.user ? req.user._id.toString() : null,
+            services
+        })
+    } catch(e) {
+        console.log(e)
+    }
 })
 
 router.get('/:id/edit', auth, async (req, res) => {
@@ -20,24 +29,45 @@ router.get('/:id/edit', auth, async (req, res) => {
         return res.redirect('/')
     }
 
-    const service = await Service.findById(req.params.id)
-
-    res.render('service-edit', {
-        title: `Edit ${service.title}`,
-        service
-    })
+    try {
+        const service = await Service.findById(req.params.id)
+        if(!isOwner(service, req)) {
+            return res.redirect('/services')
+        }
+        res.render('service-edit', {
+            title: `Edit ${service.title}`,
+            service
+        })
+    } catch(e) {
+        console.log(e)
+    }
 })
 
 router.post('/edit', auth, async (req, res) => {
-    const {id} = req.body
-    delete req.body.id
-    await Service.findByIdAndUpdate(id, req.body)
-    res.redirect('/services')
+    try {
+        const {id} = req.body
+        delete req.body.id
+
+        const service = await Service.findById(id)
+
+        if(!isOwner(service, req)) {
+            return res.redirect('/services')
+        }
+
+        Object.assign(service, req.body)
+        await service.save()
+        res.redirect('/services')
+    } catch(e) {
+        console.log(e)
+    }
 })
 
 router.post('/remove', auth, async (req, res) => {
     try {
-        await Service.deleteOne({ _id: req.body.id })
+        await Service.deleteOne({
+            _id: req.body.id,
+            userId: req.user._id
+        })
         res.redirect('/services')
     } catch(e) {
         console.log(e)
@@ -45,12 +75,16 @@ router.post('/remove', auth, async (req, res) => {
 })
 
 router.get('/:id', async (req, res) => {
-    const service = await Service.findById(req.params.id)
-    res.render('service', {
-        layout: 'empty',
-        title: `Service ${service.title}`,
-        service
-    })
+    try {
+        const service = await Service.findById(req.params.id)
+        res.render('service', {
+            layout: 'empty',
+            title: `Service ${service.title}`,
+            service
+        })
+    } catch(e) {
+        console.log(e)
+    }
 })
 
 module.exports = router
